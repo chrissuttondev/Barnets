47e90b9b2487bce9841b186ac2e5ba42624bc6ea
Chris Sutton <chrissutton1789@gmail.com>
2024-06-03 06:30:39 +0000

    Added basic HttpResponse view to home app to test server

--- barnets/settings.py
+++ barnets/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-chrissuttondev-barnets-schmv132r26.ws-eu114.gitpod.io']
 
@@ -41,7 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appointments',
     'home',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+print(LOGIN_REDIRECT_URL)
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'barnets.urls'
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
     """view to display the home page"""
-    return render(request, 'home/index.html')
+    return HttpResponse("Hello world")
